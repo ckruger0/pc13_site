ecc47b5511e153b8bef88c9af747325335d546a8
Hemanth Kondapalli <kondapallih92@gmail.com>
2018-11-15 17:50:59 -0500

    landing pages, few screens

--- app.py
+++ app.py
@@ -18,6 +18,13 @@ def submit():
 	'''
 	return render_template('signup.html')
 
+@app.route('/user')
+def user():
+	'''
+	Home page
+	'''
+	return render_template('user.html')
+
 if __name__ == '__main__':
 	port = int(os.environ.get("PORT", 5000))
 	app.run(debug=True, host='0.0.0.0', port=port)
