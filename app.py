d2e27518e1f850a89d3b1009a7667d5761f2f3fe
Hemanth Kondapalli <kondapallih92@gmail.com>
2018-11-15 16:43:44 -0500

    updates

---
+++ app.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask, request, render_template
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def index():
+	'''
+	Home page
+	'''
+	return render_template('index.html')
+
+@app.route('/signup')
+def submit():
+	'''
+	Home page
+	'''
+	return render_template('signup.html')
+
+if __name__ == '__main__':
+	port = int(os.environ.get("PORT", 5000))
+	app.run(debug=True, host='0.0.0.0', port=port)
